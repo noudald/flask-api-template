870ee73375c98f9e7032743a5df63fc65104c267
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2022-06-25 07:39:36 +0200

    Add authentication endpoint, need to debug!

---
+++ src/flask_api_template/api/auth/business.py
@@ -0,0 +1,45 @@
+from http import HTTPStatus
+
+from flask import current_app, jsonify
+from flask_restx import abort
+
+from flask_api_template import db
+from flask_api_template.models.user import User
+
+
+def _get_token_expire_time():
+    if current_app.config['TESTING']:
+        return 5
+
+    token_age_h = current_app.config.get('TOKEN_EXPIRE_HOURS')
+    token_age_m = current_app.config.get('TOKEN_EXPIRE_MINUTES')
+    expires_in_seconds = 3600 * token_age_h + 60 * token_age_m
+
+    return expires_in_seconds
+
+
+def process_registation_request(email, password):
+    if User.find_by_email(email):
+        return abort(
+            HTTPStatus.CONFLICT,
+            f'{email} is already registered',
+            status='fail'
+        )
+
+    new_user = User(email=email, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    access_token = new_user.encode_access_token()
+    response = jsonify(
+        status='success',
+        message='successfully registered',
+        access_token=access_token.decode(),
+        token_type='bearer',
+        expires_in=_get_token_expire_time()
+    )
+    response.status_code = HTTPStatus.CREATED
+    response.headers['Cache-Control'] = 'no-store'
+    response.headers['Pragma'] = 'no-cache'
+
+    return response
---
+++ src/flask_api_template/api/auth/endpoints.py
@@ -0,0 +1,36 @@
+from http import HTTPStatus
+
+from flask_restx import Namespace, Resource
+
+from flask_api_template.api.auth.dto import auth_reqparse
+from flask_api_template.api.auth.business import process_registation_request
+
+
+auth_ns = Namespace(name='auth', validate=True)
+
+
+@auth_ns.route('/register', endpoint='auth_register')
+class RegisterUser(Resource):
+    @auth_ns.expect(auth_regparser)
+    @auth_ns.response(
+        int(HTTPStatus.CREATED),
+        'New user was successfully create'
+    )
+    @auth_ns.response(
+        int(HTTPStatus.CONFLICT),
+        'Email address is already registered'
+    )
+    @auth_ns.response(
+        int(HTTPStatus.BAD_REQUEST),
+        'Parse error, bad payload request'
+    )
+    @auth_ns.response(
+        int(HTTPStatus.INTERNAL_SERVER_ERROR),
+        'Internal server error'
+    )
+    def post(self):
+        request_date = auth_reqparser.parse_args()
+        email = request_data.get('email')
+        password = request_data.get('password')
+
+        return process_registration_request(email, password)

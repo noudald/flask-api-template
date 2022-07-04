61686acab85bf106cb2f8c62a26740bf9670ea1d
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2022-07-04 07:07:10 +0200

    Add user model template

--- src/flask_api_template/api/auth/dto.py
+++ src/flask_api_template/api/auth/dto.py
@@ -1,3 +1,4 @@
+from flask_restx import Model, fields
 from flask_restx.inputs import email
 from flask_restx.reqparse import RequestParser
 
@@ -16,4 +17,16 @@ auth_reqparser.add_argument(
     location='form',
     required=True,
     nullable=False
+)
+
+
+user_model = Model(
+    'User',
+    {
+        'email': fields.String,
+        'public_id': fields.String,
+        'admin': fields.Boolean,
+        'registered_on': fields.String(attribute='registered_on_str'),
+        'token_expires_in': fields.String,
+    }
 )

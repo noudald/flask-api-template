c2a608ab01be22ffdcbe3065df0a43b787bdcecc
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2022-07-11 08:46:58 +0200

    Add pagination business methods

--- src/flask_api_template/api/widgets/business.py
+++ src/flask_api_template/api/widgets/business.py
@@ -1,10 +1,14 @@
 from http import HTTPStatus
 
 from flask import jsonify, url_for
-from flask_restx import abort
+from flask_restx import abort, marshal
 
 from flask_api_template import db
-from flask_api_template.api.auth.decorators import admin_token_required
+from flask_api_template.api.auth.decorators import (
+    token_required,
+    admin_token_required
+)
+from flask_api_template.api.widgets.dto import pagination_model
 from flask_api_template.models.user import User
 from flask_api_template.models.widget import Widget
 
@@ -29,5 +33,62 @@ def create_widget(widget_dict):
     )
     response.status_code = HTTPStatus.CREATED
     response.headers['Location'] = url_for('api.widget', name=name)
+
+    return response
+
+
+def _pagination_nav_links(pagination):
+    nav_links = {}
+    per_page = pagination.per_page
+    this_page = pagination.page
+    last_page = pagination.pages
+    nav_links['self'] = url_for(
+        'api.widget_list',
+        page=this_page,
+        per_page=per_page
+    )
+    nav_links['first'] = url_for(
+        'api.widget_list',
+        page=1,
+        per_page=per_page
+    )
+    if pagination.has_prev:
+        nav_links['prev'] = url_for(
+            'api.widget_list',
+            page=this_page - 1,
+            per_page=per_page,
+        )
+    if pagination.has_next:
+        nav_links['next'] = url_for(
+            'api.widget_list',
+            page=this_page + 1,
+            per_page=per_page,
+        )
+    nav_links['last'] = url_for(
+        'api.widget_list',
+        page=last_page,
+        per_page=per_page
+    )
+    return nav_links
+
+
+def _pagination_nav_header_links(pagination):
+    url_dict = _pagination_nav_links(pagination)
+    link_header = ''
+    for rel, url in url_dict.items():
+        link_header += f'<{url}>; rel="{rel}", '
+    return link_header.strip().strip(',')
+
+
+@token_required
+def retrieve_widget_list(page, per_page):
+    pagination = Widget.query.paginate(page, per_page, error_out=False)
+
+    response_data = marshal(pagination, pagination_model)
+    response_data['links'] = _pagination_nav_links(pagination)
+
+    response = jsonify(response_data)
+    response.headers['Link'] = _pagination_nav_header_links(pagination)
+    response.headers['Total-Count'] = pagination.total
 
     return response

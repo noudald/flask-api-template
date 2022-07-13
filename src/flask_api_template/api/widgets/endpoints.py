cd4b44faa5b22a4246d85275885cbe21237515e6
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2022-07-13 06:46:20 +0200

    Add endpoint for retrieve widget list

--- src/flask_api_template/api/widgets/endpoints.py
+++ src/flask_api_template/api/widgets/endpoints.py
@@ -2,10 +2,24 @@ from http import HTTPStatus
 
 from flask_restx import Namespace, Resource
 
-from flask_api_template.api.widgets.dto import create_widget_reqparser
-from flask_api_template.api.widgets.business import create_widget
+from flask_api_template.api.widgets.dto import (
+    create_widget_reqparser,
+    pagination_reqparser,
+    widget_owner_model,
+    widget_model,
+    pagination_links_model,
+    pagination_model,
+)
+from flask_api_template.api.widgets.business import (
+    create_widget,
+    retrieve_widget_list
+)
 
 widget_ns = Namespace(name='widgets', validate=True)
+widget_ns.models[widget_owner_model.name] = widget_owner_model
+widget_ns.models[widget_model.name] = widget_model
+widget_ns.models[pagination_links_model.name] = pagination_links_model
+widget_ns.models[pagination_model.name] = pagination_model
 
 
 @widget_ns.route('', endpoint='widget_list')
@@ -16,6 +30,19 @@ widget_ns = Namespace(name='widgets', validate=True)
     'Internal server error.'
 )
 class WidgetList(Resource):
+    @widget_ns.doc(security='Bearer')
+    @widget_ns.response(
+        int(HTTPStatus.OK),
+        'Retrieve widget list.',
+        pagination_model
+    )
+    @widget_ns.expect(pagination_reqparser)
+    def get(self):
+        request_data = pagination_reqparser.parse_args()
+        page = request_data.get('page')
+        per_page = request_data.get('per_page')
+        return retrieve_widget_list(page, per_page)
+
     @widget_ns.doc(security='Bearer')
     @widget_ns.response(int(HTTPStatus.CREATED), 'Added new widget.')
     @widget_ns.response(
